492e110d36c98228426cff7be1489991b4fd6aa1
ailenrmansilla <ailen.r.mansilla@gmail.com>
2024-02-20 02:34:58 -0500

    layout when session is found

--- shareFamilyRecipes/src/app/page.tsx
+++ shareFamilyRecipes/src/app/page.tsx
@@ -10,15 +10,3 @@ export default function Home() {
   );
 }
 
-// import { SessionProvider } from 'next-auth/react';
-// import { AppProps } from 'next/app';
-
-// const App = ({ Component, pageProps }: AppProps) => {
-//   return (
-//     <SessionProvider session={pageProps.session}>
-//       <Component {...pageProps} />
-//     </SessionProvider>
-//   );
-// };
-
-// export default App;
